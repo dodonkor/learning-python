e77556bffc98896210202090e6c5d460a8e19a13
Derrick Odonkor <odonkor.derrick@gmail.com>
2021-08-30 22:52:43 +0200

    Done with the basics

---
+++ main.py
@@ -0,0 +1,51 @@
+# Variables
+
+# name = "Derrick"  # string
+# age: int = 20
+
+# name, age = "Derrick", 20
+# pi = 3.14
+# numbers = [1,2,3,4]
+#
+# print(name)
+# print(age)
+# print(pi)
+# print(numbers)
+
+# NAMING VARIABLES
+
+# name = "James"
+# fullName = "J James"
+# PI = 3.14
+
+# DATATYPES
+# brand = "Derrick"
+# age = 2
+# pi = 3.14
+# numbers = []
+# isAdult = True
+#
+#
+# print(type(brand))
+# print(type(pi))
+# print(type(age))
+# print(type(numbers))
+# print(type(isAdult))
+
+
+# DYNAMICALLY TYPED
+# brand: str = "Derrick"
+# isAdult: bool = False
+
+# STRINGS
+# brand = 'Derrick'
+# print(brand)
+
+# name = "Derrick"
+#
+# email = f"""
+# Hello {name},
+# how are you?
+# It was nice talking to you
+# """
+# print(email)
