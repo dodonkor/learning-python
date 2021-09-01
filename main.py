f92e34b580c777414aa68f158b08cdee2c8c8276
Derrick Odonkor <odonkor.derrick@gmail.com>
2021-09-01 15:30:47 +0200

    Operators, control flow and data structures

--- main.py
+++ main.py
@@ -49,3 +49,69 @@
 # It was nice talking to you
 # """
 # print(email)
+
+# FLOW CONTROL
+# number = 0
+# message = "positive" if number > 0 else "0 or negative"
+# print(message)
+
+# if number > 0:
+#     print(f"{number} is positive")
+# elif number == 0:
+#     print(f"{number} is zero")
+# else:
+#     print(f"{number} is negative")
+
+# DATA STRUCTURES
+# LISTS
+# numbersList = [1, 1]
+# numbers.sort()
+# numbers.reverse()
+# numbers.append(1000)
+# print(len(numbers))
+# numbers.clear()
+
+# print(5 not in numbers)
+# numbers.remove(1)
+# numbers.pop()
+# del numbers[0: 3]
+
+# SETS
+# numbersSet = {1, 2, 1, 3, 4, 3}
+# lettersSet = {"A", "a", "B", "b"}
+#
+# print(numbersList)
+# print(numbersSet)
+# print(lettersSet)
+
+# SET UNION
+# lettersA = {"A", "B", "C", "D"}
+# lettersB = {"A", "E", "F"}
+#
+# union = lettersA | lettersB
+# intersection = lettersA & lettersB
+# difference = lettersA - lettersB
+#
+# print(f"Union = {union}")
+# print(f"Intersection = {intersection}")
+# print(f"Difference = {difference}")
+
+# DICTIONARIES
+person = {
+    "name": "Derrick",
+    "age": 29,
+    "address": "USA"
+}
+
+print(person["name"])
+print(person["age"])
+print(person["address"])
+print(person.keys())
+print(person.values())
+# print(person.clear())
+person["age"] = 100
+print(person)
+
+
+
+
